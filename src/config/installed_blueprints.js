49ba5c9f6417c7ca56852a9b13ce510b79d69d47
Crowdbotics <team@crowdbotics.com>
2020-04-19 10:31:49 +0000

    add package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'UserProfile30486', human_name: 'User Profile', access_route: 'UserProfile30486'},
 { name: 'Maps30482', human_name: 'Maps', access_route: 'Maps30482', icon: 'map'},
 { name: 'Add-Item30478', human_name: 'Add-item', access_route: 'Add-Item30478'},
 { name: 'Maps30477', human_name: 'Maps', access_route: 'Maps30477', icon: 'map'},
